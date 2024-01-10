0aa28fb914dc74ad9012460b397e594e41cba763
Logan Moriniere <71821650+L-Moriniere@users.noreply.github.com>
2024-01-10 14:35:26 +0100

    8 way movement

--- scene/player.gd
+++ scene/player.gd
@@ -1,46 +1,14 @@
-extends Area2D
+extends CharacterBody2D
+
+
+const SPEED = 300.0
 
 @export var speed = 400
-var screen_size
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	screen_size = get_viewport_rect().size
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO
-
-	if Input.is_action_pressed("move_right"):
-		set_sprite_properties(-90, false, true)
-		velocity.x += 1
-	elif Input.is_action_pressed("move_left"):
-		set_sprite_properties(90, false, false)
-		velocity.x -= 1
-	elif Input.is_action_pressed("move_down"):
-		set_sprite_properties(0, false, false)
-		velocity.y += 1
-	elif Input.is_action_pressed("move_up"):
-		set_sprite_properties(0, true, false)
-		velocity.y -= 1
-	elif Input.is_action_pressed("move_up") and Input.is_action_pressed("move_right"):
-		velocity.x += 1
-		velocity.y += 1
-		set_sprite_properties(45, false, false)
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
-		
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
-	
-	
-func set_sprite_properties(degrees: int,flip_vertical: bool,flip_horizontal: bool):
-	$AnimatedSprite2D.rotation_degrees = degrees
-	$AnimatedSprite2D.flip_v = flip_vertical
-	$AnimatedSprite2D.flip_h = flip_horizontal
-		
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
